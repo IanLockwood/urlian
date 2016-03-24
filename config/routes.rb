11721fbb309f439558ac867c70585ba42da0271e
IanLockwood <mrianlockwood@gmail.com>
2016-03-24 16:39:41 -0400

    Add routes and controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'urls#new'
+
+  get 'urls' => 'urls#index'
+  get 'urls/new' => 'urls#new'
+  post 'urls' => 'urls#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
